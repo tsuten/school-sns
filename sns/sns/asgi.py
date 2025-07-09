50d2e76f718abbe2e2d8633f4003712ef1958d9b
Tsuten <hiimhereforyouguys@gmail.com>
2025-07-09 16:34:28 +0900

    feat(backend): 通知関連のchannels consumerを実装

--- sns/sns/asgi.py
+++ sns/sns/asgi.py
@@ -14,7 +14,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from circle.consumers import CircleChatConsumer, CircleNotificationConsumer
-from notifications.consumers import TestConsumer
+from notifications.consumers import TestConsumer, NotificationConsumer
+from sns.utils.websocket_auth import JWTAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sns.settings')
 
@@ -22,11 +23,12 @@ websocket_urlpatterns = [
     path('ws/circle/<circle_id>/chat/', CircleChatConsumer.as_asgi()),
     path('ws/circle/<circle_id>/notifications/', CircleNotificationConsumer.as_asgi()),
     path('ws/test/', TestConsumer.as_asgi()),
+    path('ws/notification', NotificationConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
 })
