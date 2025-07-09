331da003cba9d4f7860cf9be0784a7b9fcd3462b
Tsuten <hiimhereforyouguys@gmail.com>
2025-07-09 12:22:07 +0900

    feat(backend): 通知機能を実装

--- sns/notifications/models.py
+++ sns/notifications/models.py
@@ -7,4 +7,6 @@ from django.conf import settings
 class Notification(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, unique=True, default=uuid.uuid4)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(default="")
+    is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
--- sns/sns/settings.py
+++ sns/sns/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'emojis',
     'enrollments',
     'announcement',
+    'notifications',
 ]
 
 MIDDLEWARE = [
