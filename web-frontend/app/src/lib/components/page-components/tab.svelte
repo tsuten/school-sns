4a9f998c081b9f324d5e65d61d14f74be59339eb
rui2402110 <tky2402110@stu.o-hara.ac.jp>
2025-07-10 15:56:42 +0900

    タブ部品　実装

---
+++ web-frontend/app/src/lib/components/page-components/tab.svelte
@@ -0,0 +1,12 @@
+<script>
+    // propで親からタブで切り替える先のデータを受け取る
+    // それを基にボタンでタブを切り替える処理を書く
+    let { tabsData } = $props();
+</script>
+<div class="flex justify-around">
+    {#each tabsData as t}
+        <a href="{t.href}"><button>{t.label}</button></a>
+    {/each}
+
+    
+</div>
