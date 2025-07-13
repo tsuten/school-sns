c39c8310c88c83377665ebe9e267ab2a51777bb5
Tsuten <hiimhereforyouguys@gmail.com>
2025-07-13 10:15:27 +0900

    fix(frontend): 高さがページの内容によって左右される問題を解決 スクロールするようにした

--- web-frontend/app/src/lib/components/utils/page.svelte
+++ web-frontend/app/src/lib/components/utils/page.svelte
@@ -1,7 +1,7 @@
 <script>
-    let { children } = $props();
+    let { children, class: className = "" } = $props();
 </script>
 
-<div class="flex flex-col gap-4 p-4">
+<div class="flex flex-col gap-4 p-4 h-full overflow-y-auto {className}">
     {@render children()}
 </div>
